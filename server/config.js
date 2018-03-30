8d85c5d27a1f37aa24c0b74821ec0a87a6f9afec
dhwani-lanetteam <dhwani.lanetteam@gmail.com>
2018-03-30 12:56:45 +0530

    Node API Running on http://localhost:8083/api

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  AUTH0_DOMAIN: 'dhwani-lanetteam.auth0.com', // e.g., kmaida.auth0.com
+  AUTH0_API_AUDIENCE: 'http://localhost:8083/api/', // e.g., 'http://localhost:8083/api/'
+  MONGO_URI: process.env.MONGO_URI || 'mongodb://dhwani:dhwani@ds227199.mlab.com:27199/mean'
+};
